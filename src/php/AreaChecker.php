0ee7875a24112c6ab6cd5e6366d7bfee3f1974c0
maxbarsukov <maximbarsukov@bk.ru>
2023-08-31 15:06:28 +0300

    feat: :sparkles: create area validator and checker

---
+++ src/php/AreaChecker.php
@@ -0,0 +1,21 @@
+<?php
+
+class AreaChecker {
+    public static function isInArea($x, $y, $r) {
+        // Triangle in top-right quadrant
+        if ($x >= 0 && $y >= 0) {
+            return ($x <= $r / 2) && ($y <= $r) && (2*$x + $y <= $r);
+        }
+        // Rectangle in top-left quadrant
+        if ($x < 0 && $y >= 0) {
+            return ($x >= -$r / 2) && ($y <= $r);
+        }
+        // Circle in bottom-right quadrant
+        if ($x >= 0 && $y < 0) {
+            return ($x * $x + $y * $y) <= ($r * $r);
+        }
+
+        // For bottom-left quadrant, always return false
+        return false;
+    }
+}
---
+++ src/php/CoordinatesValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+class CoordinatesValidator {
+    private $x;
+    private $y;
+    private $r;
+
+    public function __construct($x, $y, $r) {
+      $this->x = $x;
+      $this->y = $y;
+      $this->r = $r;
+    }
+
+    public function checkData() {
+        return checkX() && checkY() && checkR();
+    }
+
+    private function checkX() {
+        return in_array($this->x, array(-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2));
+    }
+
+    private function checkY() {
+        return is_numeric($this->y) && ($this->y > -3 && $this->y < 5);
+    }
+
+    private function checkR() {
+        return in_array($this->r, array(1, 1.5, 2, 2.5, 3));
+    }
+}
